fd133a49fbc7fc3a468c30bc7a178e920ad08ac7
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-22 15:45:31 -0300

    adiciona TAD lista

--- src/lista.c
+++ src/lista.c
@@ -1,116 +1,136 @@
 #include "../include/lista.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
-    int inicializa_lista(Lista_Ordenada *l, int tamanho_estrutura_dados)
-{
+int inicializa_lista(Lista_Ordenada *l, int tamanho_estrutura_dados) {
     l->primeiro = NULL;
     l->tamanho_estrutura_dados = tamanho_estrutura_dados;
     l->tamanho_lista = 0;
     return 0;
 }
 
-int adiciona_em_lista(Lista_Ordenada *l, void *dados, int pos)
-{
+int adiciona_em_lista(Lista_Ordenada *l, void *dados, int pos) {
+
     // instancia estrutura para receber elemento
     Node_Lista *novo_node;
     novo_node = (Node_Lista *)malloc(sizeof(Node_Lista));
-    if (novo_node == NULL)
-    {
+    if (novo_node == NULL) {
         // erro ao alocar elemento
         return 1;
     }
     novo_node->dados = malloc(l->tamanho_estrutura_dados);
-    if (novo_node->dados == NULL)
-    {
+    if (novo_node->dados == NULL) {
         // erro ao alocar dados do elemento
         free(novo_node);
         return 1;
     }
 
-    // copia dados do argumento para novo node
+    // define propriedades do nodo
     memcpy(novo_node->dados, dados, l->tamanho_estrutura_dados);
+    novo_node->pos = pos;
+    novo_node->proximo_node = NULL;
 
     // insere node na lista
-    Node_Lista *node_atual = l->primeiro;
-    while(true){
-        if(node_atual->proximo_node == NULL){
-            node_atual->proximo_node = novo_node;
-            novo_node->proximo_node = NULL;
-            break;
-        }
-        if(pos >= node_atual->pos && pos < node_atual->proximo_node->pos){
-            Node_Lista *proximo = node_atual->proximo_node;
-            node_atual->proximo_node = novo_node;
-            novo_node->proximo_node = proximo;
-            break;
-        }
-        node_atual = node_atual->proximo_node;
-    }
+    if (l->primeiro == NULL) {
+        // se lista vazia insere na primeira posição
+        l->primeiro = novo_node;
+    } else {
+        Node_Lista *node_anterior = NULL;
+        Node_Lista *node_atual = l->primeiro;
 
+        while (true) {
+
+            if (node_atual == NULL) {
+
+                // inserir na ultima posicao
+                node_anterior->proximo_node = novo_node;
+                break;
+            }
+            if (node_anterior == NULL && pos < node_atual->pos) {
+
+                // inserir na primeira posição
+                l->primeiro = novo_node;
+
+                novo_node->proximo_node = node_atual;
+                break;
+            }
+            if (pos >= node_anterior->pos && pos < node_atual->pos) {
+
+                node_anterior->proximo_node = novo_node;
+
+                novo_node->proximo_node = node_atual;
+                break;
+            }
+
+            node_anterior = node_atual;
+            node_atual = node_atual->proximo_node;
+        }
+    }
 
     // ajusta tamanho da lista
     l->tamanho_lista++;
     return 0;
 };
 
-int remove_da_lista(Lista_Ordenada *l, void *valor, int pos)
-{
-    if (l->tamanho_lista > 0)
-    {
-        // guarda endereço do node que vai ser removido
-        Node_Lista *node_removido = l->primeiro;
+int remove_da_lista(Lista_Ordenada *l, void *valor, int pos) {
+    if (l->tamanho_lista > 0) {
+        Node_Lista *node_anterior = NULL;
+        Node_Lista *node_atual = l->primeiro;
+        while (true) {
+            if (node_atual == NULL) {
+                return 1;
+            }
+            if (node_atual->pos == pos) {
+                // guarda endereço do node que vai ser removido
+                Node_Lista *node_removido = node_atual;
 
-        // copia dados do node removido para ponteiro valor em argumento da função, se valor for NULL continua funcao sem copiar dados
-        if (valor != NULL)
-        {
-            memcpy(valor, node_removido->dados, l->tamanho_estrutura_dados);
+                // copia dados do node removido para ponteiro valor em argumento
+                // da função, se valor for NULL continua funcao sem copiar dados
+                if (valor != NULL) {
+                    memcpy(valor, node_removido->dados,
+                           l->tamanho_estrutura_dados);
+                }
+                // ajusta apontamento do node anterior para proximo node
+                if (node_anterior == NULL) {
+                    // removendo primeiro elemento
+                    l->primeiro = node_atual->proximo_node;
+                } else {
+                    node_anterior->proximo_node = node_atual->proximo_node;
+                }
+
+                // libera memoria de node_removido
+                free(node_removido->dados);
+                free(node_removido);
+
+                // ajusta tamanho da lista
+                l->tamanho_lista--;
+                break;
+            }
+            node_anterior = node_atual;
+            node_atual = node_atual->proximo_node;
         }
-
-        // redefine primeiro node da lista
-        if (l->tamanho_lista > 1)
-        {
-            l->primeiro = l->primeiro->proximo_node;
-        }
-        else
-        {
-            // se lista so tinha 1 elemento, nao vao sobrar elementos na lista apos remocao
-            l->primeiro = NULL;
-        }
-
-        // ajusta tamanho da lista
-        l->tamanho_lista--;
-
-        // libera memoria de node_removido
-        free(node_removido->dados);
-        free(node_removido);
 
         return 0;
-    }
-    else
-    {
+    } else {
         return 1;
     }
 }
 
-int limpa_lista(Lista_Ordenada *l)
-{
-    int status = 0;
-    while (l->tamanho_lista > 0)
-    {
+int limpa_lista(Lista_Ordenada *l) {
+    while (l->tamanho_lista > 0) {
         Node_Lista *node_removido = l->primeiro;
         l->primeiro = node_removido->proximo_node;
         // libera memoria de node_removido
         free(node_removido->dados);
         free(node_removido);
+        l->tamanho_lista--;
     }
     return 0;
 }
 
-int retorna_tamanho_lista(Lista_Ordenada *l, int *tam)
-{
+int retorna_tamanho_lista(Lista_Ordenada *l, int *tam) {
     *tam = l->tamanho_lista;
     return 0;
 }
