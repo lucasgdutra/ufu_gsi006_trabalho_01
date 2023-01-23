003cba5ffef65c510becfba7931564375137ab5d
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-23 11:05:10 -0300

    inclusao de menu em arquivo principal

--- apps/restaurante.c
+++ apps/restaurante.c
@@ -1,41 +1,76 @@
 /* --------------------------------------------------------------------------------------
-   Programa que implementa um sistema de atendimento de um restaurante, com cardápio, comanda e brindes de chocolates para clientes.
-   Objetivo: Trabalho de disciplina Estrutura de dados (GSI006) BSI - Facom - UFU
-   Restricoes: 
+   Programa que implementa um sistema de atendimento de um restaurante, com
+cardápio, comanda e brindes de chocolates para clientes. Objetivo: Trabalho de
+disciplina Estrutura de dados (GSI006) BSI - Facom - UFU Restricoes:
 
    Autor: Dalmo Scalon Inacio/ Lucas Gabriel Dutra de Souza
    Disciplina: Estrutura de Dados 1
    Data da ultima atualizacao: 04/01/2023
 ----------------------------------------------------------------------------------------*/
 
-#include "../include/fila.h"
-#include "../include/pilha.h"
-#include "../include/lista.h"
 #include "../include/cardapio.h"
+#include "../include/fila.h"
+#include "../include/lista.h"
+#include "../include/pilha.h"
 #include "../include/produto.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-  
-  Produto p,p1;
-  Cardapio *c;
+int inserir_itens_cardapio() {
+    printf("Em desenvolvimento\n");
+    return 0;
+}
 
-  c = (Cardapio*) malloc(sizeof(Cardapio)*2);
-  
-  inicializa_cardapio(c,2);
-  inicializa_produto(&p, 1, "macarrao", 8, 22.50);
-  adiciona_item_cardapio(c, &p);
-  inicializa_produto(&p1, 2, "feijoada", 8, 30.00);
-  adiciona_item_cardapio(c, &p1);
+int inserir_chocolates_pilha() {
+    printf("Em desenvolvimento\n");
+    return 0;
+}
 
-    
-  //listar cardapio para cliente...
-  for(int i=0; i < c->tam_cardapio; i++){
-    printf("%s\n", c->lista[i]->nome);
-  }
+int incluir_clientes_fila() {
+    printf("Em desenvolvimento\n");
+    return 0;
+}
 
+int print_menu(int *opcao) {
+	printf("\n");
+    printf("Escolha uma das opções do menu\n");
+    printf("0 - Sair do programa\n");
+    printf("1 - Inserir itens no cardapio\n");
+    printf("2 - Inserir chocolates na pilha\n");
+    printf("3 - Incluir clientes na fila\n");
+    if(scanf("%d", opcao) == 1) return 0;
+	return 1;
+}
 
+int menu() {
+    int opcao;
+
+    while (true) {
+        print_menu(&opcao);
+        switch (opcao) {
+        case 0:
+			printf("saindo...\n");
+            return 0;
+            break;
+        case 1:
+            inserir_itens_cardapio();
+            break;
+        case 2:
+            inserir_chocolates_pilha();
+            break;
+        case 3:
+            incluir_clientes_fila();
+            break;
+        default:
+            printf("Opcao invalida\n");
+            break;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    menu();
     return 0;
 }
