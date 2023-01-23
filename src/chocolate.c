9aaff724650879f358da1f30647dbf5c0bdff8c3
dalmo123 <66819868+dalmo123@users.noreply.github.com>
2023-01-23 16:58:41 -0300

    Update chocolate.c

--- src/chocolate.c
+++ src/chocolate.c
@@ -1,1 +1,20 @@
 #include "../include/chocolate.h"
+#include "../include/pilha.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int empilha_chocolate(char *nome){
+
+    Pilha *p;
+    int tam = 5;
+    char *valor;
+
+    p = (Pilha *) malloc(sizeof(Pilha));
+    inicializa_pilha(p,tam);
+
+    empilha(p, nome);
+    printf("EMPILHADO\n");
+    
+}
+
