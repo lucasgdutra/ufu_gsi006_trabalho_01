614245256a25a7031e06abddace691642ed57918
dalmo123 <66819868+dalmo123@users.noreply.github.com>
2023-01-24 14:00:35 -0300

    Update chocolate.c

--- src/chocolate.c
+++ src/chocolate.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int inicializa_chocolate(Chocolate *c, char *nome, int tamanho_nome) {
+int inicializa_chocolate(Chocolate *c, char *nome, int tamanho_nome)
+{
     c->nome = nome;
     c->tamanho_nome = tamanho_nome;
     return 0;
 };
 
-int inicializa_pilha_chocolate(Pilha_Chocolate *pc) {
+int inicializa_pilha_chocolate(Pilha_Chocolate *pc)
+{
     Pilha *p;
     p = malloc(sizeof(Pilha));
     inicializa_pilha(p, sizeof(Chocolate));
@@ -17,15 +19,33 @@ int inicializa_pilha_chocolate(Pilha_Chocolate *pc) {
     return 0;
 };
 
-int empilha_chocolate(Pilha_Chocolate *pc, Chocolate *c) {
-    int s = empilha(pc->pilha, c);
-    if(s != 0){
-        return 1;
-    }
+int empilha_chocolate(Pilha_Chocolate *pc, Chocolate *c)
+{
+    empilha(pc->pilha, c);
     return 0;
 }
 
-int desempilha_chocolate(Pilha_Chocolate *pc, Chocolate *choco_retorno) {
+int desempilha_chocolate(Pilha_Chocolate *pc, Chocolate *choco_retorno)
+{
     desempilha(pc->pilha, choco_retorno);
     return 0;
+}
+
+void imprime_chocolate(Pilha_Chocolate *pc)
+{
+
+    // verifica se a pilha ta vazia..
+    if (pc->pilha->tamanho_pilha == 0)
+    {
+        printf("Pilha vazia...\n");
+    }
+
+    // varre a pilha pelo topo e depois passa para o proximo node até chegar no NULL
+    Node_Pilha *current = pc->pilha->topo;
+    while (current != NULL)
+    {
+        printf("%s \n", ((Chocolate *)current->dados)->nome);
+        current = current->proximo_node;
+    }
+    printf("\n");
 }
