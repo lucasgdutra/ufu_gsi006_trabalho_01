277a6ea073a92406e5d5a8208449798c83209f9c
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-28 23:01:10 -0300

    altera nome variaveis, testa ponteiro valido

--- src/cardapio.c
+++ src/cardapio.c
@@ -1,41 +1,57 @@
 #include "../include/cardapio.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-int inicializa_cardapio(Cardapio *c) {
-    Lista_Ordenada *l;
-    l = (Lista_Ordenada*)malloc(sizeof(Lista_Ordenada));
-    inicializa_lista(l, sizeof(Produto));
-    c->lista = l;
-    return 0;
+int inicializa_cardapio(Cardapio *cardapio) {
+	// testa se ponteiro eh valido
+	if (cardapio == NULL) {
+		return 1;
+	}
+	Lista_Ordenada *l;
+	l = (Lista_Ordenada *)malloc(sizeof(Lista_Ordenada));
+	inicializa_lista(l, sizeof(Produto));
+	cardapio->lista = l;
+	return 0;
 }
 
-int adiciona_item_cardapio(Cardapio *c, Produto *p) {
-    int status = adiciona_em_lista(c->lista, p, p->id);
-    if (status != 0)
-        return 1;
-    return 0;
+int adiciona_item_cardapio(Cardapio *cardapio, Produto *produto) {
+	// testa se ponteiro eh valido
+	if (cardapio == NULL || produto == NULL) {
+		return 1;
+	}
+	int status = adiciona_em_lista(cardapio->lista, produto, produto->id);
+	if (status != 0)
+		return 1;
+	return 0;
 }
 
-int remove_item_cardapio(Cardapio *c, Produto *retorno_produto, int id) {
-    int status = remove_da_lista(c->lista, retorno_produto, id);
-    if (status != 0)
-        return 1;
-    return 0;
+int remove_item_cardapio(Cardapio *cardapio, Produto *retorno_produto, int id) {
+	// testa se ponteiro eh valido
+	if (cardapio == NULL || retorno_produto == NULL) {
+		return 1;
+	}
+	int status = remove_da_lista(cardapio->lista, retorno_produto, id);
+	if (status != 0)
+		return 1;
+	return 0;
 }
 
-void mostrar_cardapio(Cardapio *c) {
-
-    // verifica se está vazio o cardapio..
-    if(c->lista->tamanho_lista == 0){
-        printf("está vazia..\n");
-    }
-    Produto p;
-    Node_Lista *node_atual = c->lista->primeiro;
-    while (node_atual != NULL) {
-        memcpy(&p, node_atual->dados, c->lista->tamanho_estrutura_dados);
-        printf("ID: %d, Nome: %s, Preco: %.2f\n", p.id, p.nome, p.preco);
-        node_atual = node_atual->proximo_node;
-    }
+int mostrar_cardapio(Cardapio *cardapio) {
+	// testa se ponteiro eh valido
+	if (cardapio == NULL) {
+		return 1;
+	}
+	// verifica se está vazio o cardapio..
+	if (cardapio->lista->tamanho_lista == 0) {
+		printf("está vazia..\n");
+	}
+	Produto p;
+	Node_Lista *node_atual = cardapio->lista->primeiro;
+	while (node_atual != NULL) {
+		memcpy(&p, node_atual->dados, cardapio->lista->tamanho_estrutura_dados);
+		printf("ID: %d, Nome: %s, Preco: %.2f\n", p.id, p.nome, p.preco);
+		node_atual = node_atual->proximo_node;
+	}
+	return 0;
 }
