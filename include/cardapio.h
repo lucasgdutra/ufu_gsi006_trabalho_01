62f85706b3981b7b75a135262c7ee40594cf4e7d
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-29 00:03:41 -0300

    altera nome variaveis

--- include/cardapio.h
+++ include/cardapio.h
@@ -8,12 +8,12 @@ typedef struct cardapio {
 	Lista_Ordenada *lista;
 } Cardapio;
 
-int inicializa_cardapio(Cardapio *c);
+int inicializa_cardapio(Cardapio *cardapio);
 
-int adiciona_item_cardapio(Cardapio *c, Produto *p);
+int adiciona_item_cardapio(Cardapio *cardapio, Produto *p);
 
-int remove_item_cardapio(Cardapio *c, Produto *retorno_produto, int id);
+int remove_item_cardapio(Cardapio *cardapio, Produto *retorno_produto, int id);
 
-int mostrar_cardapio(Cardapio *c);
+int mostrar_cardapio(Cardapio *cardapio);
 
 #endif
