9dad299e1990516a7608d2764947d69fbbe33782
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-28 23:01:42 -0300

    altera tipo funcao

--- include/chocolate.h
+++ include/chocolate.h
@@ -3,17 +3,15 @@
 
 #include "./pilha.h"
 
-typedef struct chocolate
-{
-    char *nome;
-    int tamanho_nome;
+typedef struct chocolate {
+	char *nome;
+	int tamanho_nome;
 } Chocolate;
 
 int inicializa_chocolate(Chocolate *c, char *nome, int tamanho_nome);
 
-
 typedef struct pilha_chocolate {
-    Pilha *pilha;
+	Pilha *pilha;
 } Pilha_Chocolate;
 
 int inicializa_pilha_chocolate(Pilha_Chocolate *pc);
@@ -22,6 +20,6 @@ int empilha_chocolate(Pilha_Chocolate *pc, Chocolate *c);
 
 int desempilha_chocolate(Pilha_Chocolate *pc, Chocolate *choco_retorno);
 
-void imprime_chocolate(Pilha_Chocolate *pc);
+int imprime_chocolate(Pilha_Chocolate *pc);
 
 #endif
