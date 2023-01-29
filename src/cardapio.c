5b89582b8e63e1fd8c7d8a3b087627908f158d4d
dalmo123 <66819868+dalmo123@users.noreply.github.com>
2023-01-29 17:52:09 -0300

    Update cardapio.c

--- src/cardapio.c
+++ src/cardapio.c
@@ -1,25 +1,8 @@
-/* --------------------------------------------------------------------------------------
-   Nesse arquivo temos a construçao do cardapio do nosso restaurante.
-   Objetivo: Trabalho da disciplina Estrutura de dados (GSI006) BSI - Facom -
-UFU Restricoes:
-
-   Autor: Dalmo Scalon Inacio/ Lucas Gabriel Dutra de Souza
-   Disciplina: Estrutura de Dados 1
-   Data da ultima atualizacao: 29/01/2023
-----------------------------------------------------------------------------------------*/
-
 #include "../include/cardapio.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/*
- Nome: inicializa_cardapio
- Entrada: Ponteiro da struct Cardapio que contem uma lista_ordenada
- Processo: Função verifica se o cardapio passado foi alocado, em seguida aloca a
- lista na memoria e passa a lista alocada para o cardapio recebido. Saida:
- return 0 caso dê certo, e 1 caso dê errado.
- */
 int inicializa_cardapio(Cardapio *cardapio) {
 	// testa se ponteiro eh valido
 	if (cardapio == NULL) {
@@ -32,13 +15,6 @@ int inicializa_cardapio(Cardapio *cardapio) {
 	return 0;
 }
 
-/*
- Nome: adiciona_item_cardapio
- Entrada:Ponteiro do tipo Cardapio e ponteiro do tipo Produto.
- Processo: verifica se cardapio ou produto está alocado, posteriormente adiciona
- na lista do cardapio um produto Saida: retorna 1 em caso de erro e 0 em caso de
- sucesso.
- */
 int adiciona_item_cardapio(Cardapio *cardapio, Produto *produto) {
 
 	if (cardapio == NULL || produto == NULL) {
@@ -50,13 +26,6 @@ int adiciona_item_cardapio(Cardapio *cardapio, Produto *produto) {
 	return 0;
 }
 
-/*
- Nome: remove_item_cardapio
- Entrada: Ponteiro do tipo Cardapio e ponteiro do tipo Produto e um inteiro
- indicando o id. Processo: Verifica se cardapio e produto existem na memória e
- posteriormente remove um elemento da lista do Cardapio atraves do ID. Saida:
- retorna 1 em caso de erro e 0 em caso de sucesso.
- */
 int remove_item_cardapio(Cardapio *cardapio, Produto *retorno_produto, int id) {
 
 	if (cardapio == NULL || retorno_produto == NULL) {
@@ -68,13 +37,6 @@ int remove_item_cardapio(Cardapio *cardapio, Produto *retorno_produto, int id) {
 	return 0;
 }
 
-/*
- Nome: retorna_item_cardapio
- Entrada: Ponteiro do tipo Cardapio e ponteiro de inteiro que receberá o tamanho
- retornado. Processo:Verifica se ponteiro eh valido, posteriormente, *retorno
- tamanho recebe o tamanho da lista de cardapio. Saida: retorna 1 em caso de erro
- e 0 em caso de sucesso.
- */
 int retorna_tamanho_cardapio(Cardapio *cardapio, int *retorno_tamanho) {
 
 	if (cardapio == NULL || retorno_tamanho == NULL) {
@@ -84,13 +46,6 @@ int retorna_tamanho_cardapio(Cardapio *cardapio, int *retorno_tamanho) {
 	return 0;
 }
 
-/*
- Nome: buscar_item_cardapio
- Entrada: Ponteiro de Cardapio, ponteiro do tipo Produto, e um int indicando
- posicao Processo:Primeiro os ponteiros sao testados se estao validos,
- posteriormente, eh buscado na lista atraves da posicao, e é retornado um
- Produto. Saida: retorna 1 em caso de erro e 0 em caso de sucesso.
- */
 int buscar_item_cardapio(Cardapio *cardapio, int posicao,
 						 Produto *retorno_produto) {
 
