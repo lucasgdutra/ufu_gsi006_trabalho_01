fa6a9fa69ea223831bb6f37c90f3fd8ad5b4dfea
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-28 23:04:08 -0300

    Revert "altera tipo funcao"
    This reverts commit 6d98086f7620d34f2e33caaca76ce5ae32db9b3b.

--- apps/restaurante.c
+++ apps/restaurante.c
@@ -10,7 +10,6 @@ disciplina Estrutura de dados (GSI006) BSI - Facom - UFU Restricoes:
 
 #include "../include/cardapio.h"
 #include "../include/chocolate.h"
-#include "../include/cliente.h"
 #include "../include/fila.h"
 #include "../include/lista.h"
 #include "../include/pilha.h"
@@ -54,10 +53,6 @@ int leitura_produto(Produto *produto_retorno) {
 }
 
 int inserir_itens_cardapio(Cardapio *c) {
-	// testa se ponteiro eh valido
-	if (cardapio == NULL) {
-		return 1;
-	}
 	int status;
 	Produto *p;
 	p = (Produto *)malloc(sizeof(Produto));
@@ -78,14 +73,8 @@ int inserir_itens_cardapio(Cardapio *c) {
 	return 0;
 }
 
-int inserir_chocolates_pilha(Pilha_Chocolate *pilha_chocolate) {
-	// testa se ponteiro eh valido
-	if (pilha_chocolate == NULL) {
-		return 1;
-	}
-	int status;
-	Chocolate *chocolate;
-	chocolate = (Chocolate *)malloc(sizeof(Chocolate));
+int inserir_chocolates_pilha(Pilha_Chocolate *pc) {
+	Chocolate c;
 	char *nome;
 	int tamanho_nome = 15;
 
@@ -99,145 +88,37 @@ int inserir_chocolates_pilha(Pilha_Chocolate *pilha_chocolate) {
 	if (scanf("%s", nome) != 1)
 		return 1;
 
-	status = inicializa_chocolate(chocolate, nome, tamanho_nome);
-	if (status != 0) {
-		printf(
-			"erro em inicializa_chocolate em funcao inserir_chocolates_pilha");
-		return 1;
-	}
-	status = empilha_chocolate(pilha_chocolate, chocolate);
-	if (status != 0) {
-		printf("erro em empilha_chocolate em funcao inserir_chocolates_pilha");
-		return 1;
-	}
-	return 0;
-}
-
-int inserir_itens_comanda(Comanda *comanda) {
-	// testa se ponteiro eh valido
-	if (comanda == NULL) {
-		return 1;
-	}
-	int status;
-	Produto *produto;
-	produto = (Produto *)malloc(sizeof(Produto));
-
-	status = leitura_produto(produto);
-	if (status != 0) {
-		printf("Erro leitura_produto em funcao inserir_itens_comanda");
-		return 1;
-	}
-	status = adiciona_item_comanda(comanda, produto);
-	if (status != 0) {
-		printf("Erro adiciona_item_cardapio em funcao inserir_itens_comanda");
-		return 1;
-	}
-	printf("item inserido na comanda..\n");
+	inicializa_chocolate(&c, nome, tamanho_nome);
+	empilha_chocolate(pc, &c);
 
 	return 0;
 }
 
-int incluir_clientes_fila(Fila_Cliente *fila_cliente) {
-	// testa se ponteiro eh valido
-	if (fila_cliente == NULL) {
-		return 1;
-	}
-	int status, tamanho_nome = 30, entrada;
-	char *nome;
-
-	Cliente *cliente;
-	Comanda *comanda;
-
-	cliente = (Cliente *)malloc(sizeof(Cliente));
-	comanda = (Comanda *)malloc(sizeof(Comanda));
-
-	nome = (char *)malloc(sizeof(char) * tamanho_nome);
-	if (nome == NULL) {
-		printf("Erro ao alocar nome em funcao incluir_clientes_fila\n");
-		return 1;
-	}
-
-	printf("Digite o nome do cliente\n");
-	scanf("%s", nome);
-
-	status = inicializa_comanda(comanda);
-	if (status != 0) {
-		printf("Erro inicializa_comanda em funcao incluir_clientes_fila");
-		return 1;
-	}
-	status = inserir_itens_comanda(comanda);
-	if (status != 0) {
-		printf("Erro inserir_itens_comanda em funcao incluir_clientes_fila");
-		return 1;
-	}
-	while (true) {
-		printf("Deseja inserir mais itens na comanda?\n");
-		printf("Digite 1 para Sim\n");
-		printf("Digite 0 para Nao\n");
-		scanf("%d", &entrada);
-		if (entrada == 0) {
-			break;
-		}
-		if (entrada == 1) {
-			status = inicializa_comanda(comanda);
-			if (status != 0) {
-				printf(
-					"Erro inicializa_comanda em funcao incluir_clientes_fila");
-				return 1;
-			}
-		} else {
-			printf("entrada invalida, entradas aceitas 0 ou 1");
-		}
-	}
-	status = inicializa_cliente(cliente, nome, tamanho_nome, comanda);
-	if (status != 0) {
-		printf("Erro inicializa_cliente em funcao incluir_clientes_fila");
-		return 1;
-	}
-	status = adiciona_cliente_fila(fila_cliente, cliente);
-	if (status != 0) {
-		printf("Erro adiciona_cliente_fila em funcao incluir_clientes_fila");
-		return 1;
-	}
-	printf("cliente inserido na fila..\n");
-
+int incluir_clientes_fila() {
+	printf("em desenvolvimento..\n");
 	return 0;
 }
 
-int atender_cliente(Fila_Cliente *fila_cliente) {
-	// testa se ponteiro eh valido
-	if (fila_cliente == NULL) {
-		return 1;
-	}
+int atender_cliente() {
+
 	printf("em desenvolvimento..\n");
 
 	return 0;
 }
 
-int imprimir_cardapio(Cardapio *cardapio) {
-	// testa se ponteiro eh valido
-	if (cardapio == NULL) {
-		return 1;
-	}
-	mostrar_cardapio(cardapio);
+int imprimir_cardapio(Cardapio *c) {
+
+	mostrar_cardapio(c);
 
 	return 0;
 }
 
-int imprimir_fila_clientes(Fila_Cliente *fila_cliente) {
-	// testa se ponteiro eh valido
-	if (fila_cliente == NULL) {
-		return 1;
-	}
+int imprimir_fila_clientes() {
 	printf("em desenvolvimento..\n");
 	return 0;
 }
 
 int print_menu(int *opcao) {
-	// testa se ponteiro eh valido
-	if (opcao == NULL) {
-		return 1;
-	}
 	printf("\n");
 	printf("Escolha uma das opções do menu\n");
 	printf("0 - Sair do programa\n");
@@ -248,19 +129,17 @@ int print_menu(int *opcao) {
 	printf("5 - Imprimir cardapio\n");
 	printf("6 - Imprimir pilha de chocolates\n");
 	printf("7 - Imprimir fila de clientes\n");
-	if (scanf("%d", opcao) != 1)
-		return 1;
-	return 0;
+	if (scanf("%d", opcao) == 1)
+		return 0;
+	return 1;
 }
 
 int main() {
-	Pilha_Chocolate pilha_chocolate;
-	Cardapio cardapio;
-	Fila_Cliente fila_cliente;
+	Pilha_Chocolate pc;
+	Cardapio c;
 
-	inicializa_pilha_chocolate(&pilha_chocolate);
-	inicializa_cardapio(&cardapio);
-	inicializa_fila_cliente(&fila_cliente);
+	inicializa_pilha_chocolate(&pc);
+	inicializa_cardapio(&c);
 
 	int opcao;
 	while (true) {
@@ -271,25 +150,25 @@ int main() {
 				return 0;
 				break;
 			case 1:
-				inserir_itens_cardapio(&cardapio);
+				inserir_itens_cardapio(&c);
 				break;
 			case 2:
-				inserir_chocolates_pilha(&pilha_chocolate);
+				inserir_chocolates_pilha(&pc);
 				break;
 			case 3:
-				incluir_clientes_fila(&fila_cliente);
+				incluir_clientes_fila();
 				break;
 			case 4:
-				atender_cliente(&fila_cliente);
+				atender_cliente();
 				break;
 			case 5:
-				imprimir_cardapio(&cardapio);
+				imprimir_cardapio(&c);
 				break;
 			case 6:
-				imprime_chocolate(&pilha_chocolate);
+				imprime_chocolate(&pc);
 				break;
 			case 7:
-				imprimir_fila_clientes(&fila_cliente);
+				imprimir_fila_clientes();
 				break;
 			default:
 				printf("Opcao invalida\n");
