9224b21842e92b7e3671c9ccb550c36957a6b281
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-28 22:45:02 -0300

    testa validade ponteiros produto

--- src/produto.c
+++ src/produto.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-int inicializa_produto(Produto *produto, int id, char *nome, int tam_nome, float preco){
+int inicializa_produto(Produto *produto, int id, char *nome, int tam_nome,
+					   float preco) {
+	// testa se ponteiro eh valido
+	if (produto == NULL || nome == NULL) {
+		return 1;
+	}
 	produto->id = id;
 
 	produto->tam_nome = tam_nome;
@@ -15,17 +20,26 @@ int inicializa_produto(Produto *produto, int id, char *nome, int tam_nome, float
 }
 
 int retorna_id_produto(Produto *produto, int *retorno_id) {
+	// testa se ponteiro eh valido
+	if (produto == NULL || retorno_id == NULL) {
+		return 1;
+	}
 	*retorno_id = produto->id;
 	return 0;
 }
 
 int retorna_nome_produto(Produto *produto, char *retorno_nome) {
+	if (produto == NULL || retorno_nome == NULL) {
+		return 1;
+	}
 	memcpy(retorno_nome, produto->nome, produto->tam_nome);
 	return 0;
 }
 
 int retorna_preco_produto(Produto *produto, float *retorno_preco) {
+	if (produto == NULL || retorno_preco == NULL) {
+		return 1;
+	}
 	*retorno_preco = produto->preco;
 	return 0;
 }
-
