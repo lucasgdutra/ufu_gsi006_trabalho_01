3d70a3986bc9ca4f8995782a5aa0e4c44db9d106
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-28 22:57:41 -0300

    altera nome variaveis, teste validade ponteiro

--- src/comanda.c
+++ src/comanda.c
@@ -1,7 +1,7 @@
 /* --------------------------------------------------------------------------------------
    Comanda
-   Objetivo: Trabalho de disciplina Estrutura de dados (GSI006) BSI - Facom - UFU
-   Restricoes:
+   Objetivo: Trabalho de disciplina Estrutura de dados (GSI006) BSI - Facom -
+UFU Restricoes:
 
    Autor: Dalmo Scalon Inacio/ Lucas Gabriel Dutra de Souza
    Disciplina: Estrutura de Dados 1
@@ -9,39 +9,46 @@
 ----------------------------------------------------------------------------------------*/
 
 #include "../include/comanda.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-int inicializa_comanda(Comanda *comanda){
-
-    Lista_Ordenada *l;
-    l = (Lista_Ordenada*)malloc(sizeof(Lista_Ordenada));
-    inicializa_lista(l, sizeof(Produto));
-    comanda->lista = l;
-    return 0;
-
+int inicializa_comanda(Comanda *comanda) {
+	// testa se ponteiro eh valido
+	if (comanda == NULL) {
+		return 1;
+	}
+	Lista_Ordenada *lista;
+	lista = (Lista_Ordenada *)malloc(sizeof(Lista_Ordenada));
+	inicializa_lista(lista, sizeof(Produto));
+	comanda->lista = lista;
+	return 0;
 }
 
-int adiciona_item_comanda(Comanda *comanda, Produto *p)
-{
+int adiciona_item_comanda(Comanda *comanda, Produto *produto) {
+	// testa se ponteiro eh valido
+	if (comanda == NULL || produto == NULL) {
+		return 1;
+	}
+	int status = adiciona_em_lista(comanda->lista, produto, produto->id);
+	if (status != 0)
+		return 1;
 
-    int status = adiciona_em_lista(comanda->lista, p, p->id);
-    if (status != 0)
-       return 1;
-
-    return 0;
+	return 0;
 }
 
 int soma_valor_comanda(Comanda *comanda, float *resultado) {
-    Node_Lista *no = comanda->lista->primeiro;
-    float soma = 0;
-    while (no != NULL) {
-        Produto *p = (Produto*)no->dados;
-        soma += p->preco;
-        no = no->proximo_node;
-    }
-    *resultado = soma;
-    return 0;
+	// testa se ponteiro eh valido
+	if (comanda == NULL || resultado == NULL) {
+		return 1;
+	}
+	Node_Lista *node_atual = comanda->lista->primeiro;
+	float soma = 0;
+	while (node_atual != NULL) {
+		Produto *p = (Produto *)node_atual->dados;
+		soma += p->preco;
+		node_atual = node_atual->proximo_node;
+	}
+	*resultado = soma;
+	return 0;
 }
-
