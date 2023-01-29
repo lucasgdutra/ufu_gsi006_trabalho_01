37d081238494f78f5cc11dd54d2353acce60eff9
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-29 17:25:53 -0300

    formatacao produto teste

--- test/produto.c
+++ test/produto.c
@@ -3,24 +3,25 @@
 #include <stdlib.h>
 #define MAX_CHAR_SIZE 100
 int main() {
-    int id;
-    float preco;
-    char nome[MAX_CHAR_SIZE];
-    while (fscanf(stdin, "%d %s %f", &id, nome, &preco) != EOF) {
-        Produto p;
-        inicializa_produto(&p, id, nome, MAX_CHAR_SIZE, preco);
+	int id;
+	float preco;
+	char nome[MAX_CHAR_SIZE];
+	while (fscanf(stdin, "%d %s %f", &id, nome, &preco) != EOF) {
+		Produto p;
+		inicializa_produto(&p, id, nome, MAX_CHAR_SIZE, preco);
 
-        int id_retorno;
-        retorna_id_produto(&p,&id_retorno);
+		int id_retorno;
+		retorna_id_produto(&p, &id_retorno);
 
-        char nome_retorno[MAX_CHAR_SIZE];
-        retorna_nome_produto(&p, nome_retorno);
+		char nome_retorno[MAX_CHAR_SIZE];
+		retorna_nome_produto(&p, nome_retorno);
 
-        float preco_retorno;
-        retorna_preco_produto(&p, &preco_retorno);
+		float preco_retorno;
+		retorna_preco_produto(&p, &preco_retorno);
 
-        printf("Id: %d, nome:%s, preco:%.2f\n", id_retorno, nome_retorno, preco_retorno);
-    }
+		printf("Id: %d, nome:%s, preco:%.2f\n", id_retorno, nome_retorno,
+			   preco_retorno);
+	}
 
-    return 0;
+	return 0;
 }
