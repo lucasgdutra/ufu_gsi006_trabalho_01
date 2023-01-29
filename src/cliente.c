04555840a18c35eddc8473526442220e9cdb0d73
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-28 22:58:57 -0300

    altera nome variaveis, testa ponteiro valido

--- src/cliente.c
+++ src/cliente.c
@@ -14,29 +14,45 @@ UFU Restricoes:
 
 int inicializa_cliente(Cliente *c, char *nome, int tamanho_nome,
 					   Comanda *comanda) {
+	// testa se ponteiro eh valido
+	if (c == NULL || comanda == NULL) {
+		return 1;
+	}
 	c->nome = nome;
 	c->tamanho_nome = tamanho_nome;
 	c->comanda = comanda;
 	return 0;
 }
 
-int inicializa_fila_cliente(Fila_Cliente *fc) {
+int inicializa_fila_cliente(Fila_Cliente *fila_cliente) {
+	// testa se ponteiro eh valido
+	if (fila_cliente == NULL) {
+		return 1;
+	}
 	Fila *fila;
 	fila = (Fila *)malloc(sizeof(Fila));
 	inicializa_fila(fila, sizeof(Cliente));
-	fc->fila = fila;
+	fila_cliente->fila = fila;
 	return 0;
 };
 
-int adiciona_cliente_fila(Fila_Cliente *fc, Cliente *c) {
-	int status = adiciona_final_fila(fc->fila,c);
+int adiciona_cliente_fila(Fila_Cliente *fila_cliente, Cliente *c) {
+	// testa se ponteiro eh valido
+	if (fila_cliente == NULL || c == NULL) {
+		return 1;
+	}
+	int status = adiciona_final_fila(fila_cliente->fila, c);
 	if (status != 0)
 		return 1;
 	return 0;
 }
 
-int remove_cliente_fila(Fila_Cliente *fc, Cliente *cliente_retorno){
-	int status = remove_inicio_fila(fc->fila,cliente_retorno);
+int remove_cliente_fila(Fila_Cliente *fila_cliente, Cliente *cliente_retorno) {
+	// testa se ponteiro eh valido
+	if (fila_cliente == NULL || cliente_retorno == NULL) {
+		return 1;
+	}
+	int status = remove_inicio_fila(fila_cliente->fila, cliente_retorno);
 	if (status != 0)
 		return 1;
 	return 0;
