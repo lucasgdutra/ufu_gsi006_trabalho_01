9cc9daa8d3823e072d150c4c28d99d695438a3aa
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-01-29 00:47:58 -0300

    formatacao, nova funcao buscar_item_lista

--- include/lista.h
+++ include/lista.h
@@ -1,7 +1,7 @@
 /* --------------------------------------------------------------------------------------
    TAD Lista_Ordenada
-   Objetivo: Trabalho de disciplina Estrutura de dados (GSI006) BSI - Facom - UFU
-   Restricoes:
+   Objetivo: Trabalho de disciplina Estrutura de dados (GSI006) BSI - Facom -
+UFU Restricoes:
 
    Autor: Dalmo Scalon Inacio/ Lucas Gabriel Dutra de Souza
    Disciplina: Estrutura de Dados 1
@@ -10,33 +10,36 @@
 #ifndef LISTA_H
 #define LISTA_H
 
-typedef struct node_lista
-{
-    int pos;
-    void *dados;
-    struct node_lista *proximo_node;
+typedef struct node_lista {
+	int pos;
+	void *dados;
+	struct node_lista *proximo_node;
 } Node_Lista;
 
-typedef struct lista
-{
-    Node_Lista *primeiro;
-    int tamanho_lista;
-    int tamanho_estrutura_dados;
+typedef struct lista {
+	Node_Lista *primeiro;
+	int tamanho_lista;
+	int tamanho_estrutura_dados;
 } Lista_Ordenada;
 
-/*inicializa estrutura lista com valores iniciais, retorna 0 se não houver erro*/
-int inicializa_lista(Lista_Ordenada *l, int tamanho_estrutura_dados);
+/*inicializa estrutura lista com valores iniciais, retorna 0 se não houver
+ * erro*/
+int inicializa_lista(Lista_Ordenada *lista, int tamanho_estrutura_dados);
 
 /*adiciona elemento ao final da lista, retorna 0 se não houver erro*/
-int adiciona_em_lista(Lista_Ordenada *l, void *dados, int pos);
+int adiciona_em_lista(Lista_Ordenada *lista, void *dados, int pos);
 
-/*remove elemento do inicio da lista e retorna valor removido pra ponteiro valor do argumento, retorna 0 se não houver erro*/
-int remove_da_lista(Lista_Ordenada *l, void *valor, int pos);
+/*remove elemento do inicio da lista e retorna valor removido pra ponteiro valor
+ * do argumento, retorna 0 se não houver erro*/
+int remove_da_lista(Lista_Ordenada *lista, void *valor, int pos);
 
 /*remove todos elementos da lista, retorna 0 se não houver erro*/
-int limpa_lista(Lista_Ordenada *l);
+int limpa_lista(Lista_Ordenada *lista);
 
-/*copia tamanho da lista para variavel tam em argumento, retorna 0 se não houver erro*/
-int retorna_tamanho_lista(Lista_Ordenada *l, int *tam);
+/*copia tamanho da lista para variavel tam em argumento, retorna 0 se não houver
+ * erro*/
+int retorna_tamanho_lista(Lista_Ordenada *lista, int *tam);
+
+int buscar_item_lista(Lista_Ordenada *lista, int posicao, void *retorno_dados);
 
 #endif
